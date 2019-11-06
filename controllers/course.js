3d31948f88479abf52d6bcb0588c8ded475801e7
Dakota Gravitt <19499852+dakotagrvtt@users.noreply.github.com>
2019-11-06 17:16:10 -0600

    Edited course data controller for views & CRUD operations

--- controllers/course.js
+++ controllers/course.js
@@ -1,27 +1,26 @@
 /**
 *  Course controller
-*  Handles requests related to developer resources.
+*  Handles requests related to course resources.
 *
 * @author Dakota Gravitt <s523439@nwmissouri.edu>
 *
 */
 const express = require('express')
 const api = express.Router()
-// const Model = require('../models/developer.js')
 const find = require('lodash.find')
-const notfoundstring = 'Could not find developer with id='
+const notfoundstring = 'Could not find course with id='
 
 // RESPOND WITH JSON DATA  --------------------------------------------
 
 // GET all JSON
-api.get('/findall', (req, res) => {
+api.get('course/findall', (req, res) => {
   res.setHeader('Content-Type', 'application/json')
   const data = req.app.locals.course.query
   res.send(JSON.stringify(data))
 })
 
 // GET one JSON by ID
-api.get('/findone/:id', (req, res) => {
+api.get('course/findone/:id', (req, res) => {
   res.setHeader('Content-Type', 'application/json')
   const id = parseInt(req.params.id)
   const data = req.app.locals.course.query
@@ -32,10 +31,85 @@ api.get('/findone/:id', (req, res) => {
 
 // RESPOND WITH VIEWS  --------------------------------------------
 
-// later
+// GET to this controller base URI (the default)
+api.get('course/', (req, res) => {
+  res.render('course/index.ejs', {
+    courses: req.app.locals.course.query
+  })
+})
+
+// GET create
+api.get('course/create', (req, res) => {
+  res.render('course/create', {
+    courses: req.app.locals.course.query,
+    course: new Model()
+  })
+})
+
+// GET /delete/:id
+api.get('course/delete/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const data = req.app.locals.course.query
+  const item = find(data, { _id: id })
+  if (!item) { return res.end(notfoundstring + id) }
+  res.render('course/delete', {
+    course: item
+  })
+})
+
+// GET /details/:id
+api.get('course/details/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const data = req.app.locals.course.query
+  const item = find(data, { _id: id })
+  if (!item) { return res.end(notfoundstring + id) }
+  res.render('course/details', {
+    course: item
+  })
+})
+
+// GET one
+api.get('course/edit/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const data = req.app.locals.course.query
+  const item = find(data, { _id: id })
+  if (!item) { return res.end(notfoundstring + id) }
+  res.render('course/edit', {
+    course: item
+  })
+})
 
 // RESPOND WITH DATA MODIFICATIONS  -------------------------------
 
-// later
+// POST new
+api.post('/save', (req, res) => {
+  console.info(`Handling POST ${req}`)
+  console.debug(JSON.stringify(req.body))
+  const item = new Model()
+  console.info(`NEW ID ${req.body._id}`)
+  item._id = parseInt(req.body._id)
+  item.course_num = req.body.course_num
+  item.sections = req.body.sections
+  item.title = req.body.title
+  item.description = req.body.description
+  item.credit_hours = req.body.credit_hours
+  res.send(`THIS FUNCTION WILL SAVE A NEW course ${JSON.stringify(item)}`)
+})
+
+// POST update with id
+api.post('/save/:id', (req, res) => {
+  console.info(`Handling SAVE request ${req}`)
+  const id = parseInt(req.params.id)
+  console.info(`Handling SAVING ID=${id}`)
+  res.send(`THIS FUNCTION WILL SAVE CHANGES TO AN EXISTING course with id=${id}`)
+})
+
+// DELETE id (uses HTML5 form method POST)
+api.post('/delete/:id', (req, res) => {
+  console.info(`Handling DELETE request ${req}`)
+  const id = parseInt(req.params.id)
+  console.info(`Handling REMOVING ID=${id}`)
+  res.send(`THIS FUNCTION WILL DELETE FOREVER THE EXISTING course with id=${id}`)
+})
 
 module.exports = api
