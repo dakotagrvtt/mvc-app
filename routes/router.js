a2cd131c8bc6a5df772e9d089a2711ac5421fc48
Dakota Gravitt <19499852+dakotagrvtt@users.noreply.github.com>
2019-11-07 13:38:27 -0600

    Add error handling and view fixes for better routing

--- routes/router.js
+++ routes/router.js
@@ -10,17 +10,16 @@
 const express = require('express')
 
 console.log('START routing')
+
 const router = express.Router()
 
 // Manage top-level request first
 router.get('/', (req, res, next) => {
-  console.log('Request to /')
-  res.sendFile('index.html')
+  res.render('index', { title: 'MVC' })
 })
 
 router.get('/index', (req, res, next) => {
-  console.log('Request to /index')
-  res.sendFile('index.html')
+  res.render('index', { title: 'MVC' })
 })
 
 // Defer path requests to a particular controller
@@ -28,5 +27,18 @@ router.use('/dev', require('../controllers/developer.js'))
 router.use('/student', require('../controllers/student.js'))
 router.use('/course', require('../controllers/course.js'))
 
-console.log('END routing')
+// catch 404 and forward to error handler
+router.use((req, res, next) => {
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
+
+// error handler
+router.use((err, req, res, next) => {
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error', { status: err.status, message: err.message })
+})
+
 module.exports = router
